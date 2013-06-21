4386cf34dbd7f8be804d32d0392b51a6772693c8
Fabian Vogelsteller <fabian@frozeman.de>
2013-06-22 00:38:10 +0200

    first commit

---
+++ app/testProject.js
@@ -0,0 +1,96 @@
+// Packages used (meteorite)
+// Meteor Router
+// go-offline (to prevent taht data gets stored persistenly) https://github.com/awwx/meteor-go-offline
+// autopublish removed
+
+
+if (Meteor.isClient) {
+
+  Session.setDefault('container','page1');
+
+  Meteor.Router.add({
+      '/': function(){
+
+
+          // -> COMMENT THIS OUT (to test only template switching)
+          // clean the database
+          OfflineCollections.clean('collection');
+          // -> COMMENT THESE OUT *END*
+
+          Session.set('container','page1');
+      },
+      '/page2': function(){
+
+          // -> COMMENT THIS OUT (to test only template switching)
+          // fill the collection, on each page visit
+          for (var i = 0; i < 100; i++) {
+            DB.collection.insert({
+              text: 'Some Text from the Collection'
+            });
+          };
+          // -> COMMENT THESE OUT *END*
+
+          Session.set('container','page2');
+      },
+
+      '*': 'not_found'
+  });
+
+
+  // template container
+  Template.container.container = function () {
+    return new Handlebars.SafeString(Template[Session.get('container')]());
+  };
+
+
+  Template.page1.greeting = function () {
+    return "Lorem ipsum Sint fugiat eiusmod aliquip irure ea dolore. Lorem ipsum Nostrud cillum ea cupidatat sint labore quis mollit culpa id. Lorem ipsum Do velit mollit dolore consequat consectetur in consectetur laborum minim minim deserunt id. Lorem ipsum Cupidatat officia sint ut Excepteur pariatur eiusmod et id anim minim Duis. Lorem ipsum Magna veniam enim veniam nulla quis occaecat dolor proident aute voluptate Duis adipisicing. Lorem ipsum Occaecat ut laboris officia magna non aute dolore nulla. Lorem ipsum Elit fugiat enim aliquip elit Excepteur ut.Lorem ipsum Adipisicing ut qui qui Lorem ipsum Cillum ad exercitation commodo aute qui voluptate in in qui. Lorem ipsum Consequat in dolor ut minim est sint voluptate do Duis commodo non. Lorem ipsum Cillum culpa velit aliquip aute aute pariatur.aliquip nisi officia dolor dolore exercitation nisi. Lorem ipsum Velit do reprehenderit mollit in eu non ut laborum sed in ut. Lorem ipsum Consequat sint anim occaecat in anim amet nulla. Lorem ipsum Dolore aliqua non occaecat fugiat consequat pariatur eiusmod cupidatat.";
+  };
+
+  Template.page2.data = function () {
+    return DB.collection.find();
+  };
+
+
+  // Template.hello.events({
+  //   'click input' : function () {
+  //     // template data, if any, is available in 'this'
+  //     if (typeof console !== 'undefined')
+  //       console.log("You pressed the button");
+  //   }
+  // });
+
+
+  Meteor.startup(function () {
+
+
+    // switch back and forth
+    setInterval(function () {
+      Meteor.Router.to('/');
+      setTimeout(function () {
+        Meteor.Router.to('/page2');
+      }, 200);
+    }, 500);
+
+
+  });
+}
+
+// create collection
+DB = {};
+DB.collection = new Meteor.Collection('collection');
+
+
+
+
+
+
+
+
+// dont need that
+
+// if (Meteor.isServer) {
+//   Meteor.startup(function () {
+//     // code to run on server at startup
+//   });
+// }
