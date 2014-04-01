211c0755126af041f632263cbb527fb26a96dae3
Fabian Vogelsteller <fabian.vogelsteller@tunedin.de>
2014-04-01 19:08:46 +0200

    created memory test for adding properties to the template instance in an event

--- README.md
+++ README.md
@@ -1,18 +1,9 @@
 MeteorMemoryTest
 ================
 
-Related issue: https://github.com/meteor/meteor/issues/1537
+Related issue: https://github.com/meteor/meteor/issues/1997
 
-This is a test i wrote to show how it stacks up memory in meteor js, when using local collection.
+Run the test and see the memory console (Chrome Developer Tools -> timeline)
+uncomment line 28 in the testProject.js to and recheck, and you will see the memory grow.
 
-What i do here is having two sites, which are switched back and forth, and you can monitor the memory using Chrome -> Dev Tools -> Timeline Tab -> button on the bottom, to record
-
-When uncommenting the insertion and removing of collection entries, it stacks up until 24MB, but seems to stay constant at around 15MB.
-Which is already a lot for haveing only two almost empty pages.
-
-When instering and removing collection data, on page switches, it stacks up memory until 170MB+.
-
-
-Screenshot
----------
-![screen shot 2013-06-21 at 22 07 16](https://f.cloud.github.com/assets/232662/689380/fbd767fa-dab0-11e2-853d-87123b3afc0d.png)
+The template instance passed to the event doesn't seem to get cleaned up properly.
--- testProject.js
+++ testProject.js
@@ -3,27 +3,13 @@ if (Meteor.isClient) {
 
   Session.setDefault('container','page1');
   var page1 = function () {
-    // -> COMMENT THIS OUT (to test only template switching)
-    // clean the database
-    _.each(DB.collection.find({}).fetch(), function(item){
-        DB.collection.remove({_id: item._id});
-    });
-    // -> COMMENT THESE OUT *END*
-
     Session.set('container','page1');
+    // View.set('switchPage','page1');
   };
 
   var page2 = function () {
-    // -> COMMENT THIS OUT (to test only template switching)
-    // fill the collection, on each page visit
-    for (var i = 0; i < 100; i++) {
-      DB.collection.insert({
-        text: 'Some Text from the Collection'
-      });
-    };
-    // -> COMMENT THESE OUT *END*
-
     Session.set('container','page2');
+    // View.set('switchPage','page2');
   };
 
 
@@ -32,14 +18,33 @@ if (Meteor.isClient) {
     return Template[Session.get('container')];
   };
 
+  Template.page1.rendered = function () {
+    // this.properties =  new Array(1000).join(new Array(2000).join("XXXXX"));
+    $(this.find('h1')).trigger('click');
+
+  };
+  Template.page1.destroyed = function () {
+    // CLEAR MEMORY LEAK
+    this.addString = null;
+  };
+
+  // use this to test that in the rendered callback it get cleaned up properly
+  // Template.page1.renderd = function () {
+  //   this.properties = new Array(1000).join(new Array(2000).join("XXXXX"));
+  // };
 
   Template.page1.greeting = function () {
     return "Lorem ipsum Sint fugiat eiusmod aliquip irure ea dolore. Lorem ipsum Nostrud cillum ea cupidatat sint labore quis mollit culpa id. Lorem ipsum Do velit mollit dolore consequat consectetur in consectetur laborum minim minim deserunt id. Lorem ipsum Cupidatat officia sint ut Excepteur pariatur eiusmod et id anim minim Duis. Lorem ipsum Magna veniam enim veniam nulla quis occaecat dolor proident aute voluptate Duis adipisicing. Lorem ipsum Occaecat ut laboris officia magna non aute dolore nulla. Lorem ipsum Elit fugiat enim aliquip elit Excepteur ut.Lorem ipsum Adipisicing ut qui qui Lorem ipsum Cillum ad exercitation commodo aute qui voluptate in in qui. Lorem ipsum Consequat in dolor ut minim est sint voluptate do Duis commodo non. Lorem ipsum Cillum culpa velit aliquip aute aute pariatur.aliquip nisi officia dolor dolore exercitation nisi. Lorem ipsum Velit do reprehenderit mollit in eu non ut laborum sed in ut. Lorem ipsum Consequat sint anim occaecat in anim amet nulla. Lorem ipsum Dolore aliqua non occaecat fugiat consequat pariatur eiusmod cupidatat.";
   };
 
-  Template.page2.data = function () {
-    return DB.collection.find();
-  };
+
+
+  Template.page1.events({
+    'click h1': function(e, template){
+      console.log('clicked');
+      template.addString = new Array(1000).join(new Array(2000).join("XXXXX"));
+    }
+  });
 
 
   // EVENTS
@@ -56,7 +61,7 @@ if (Meteor.isClient) {
       var startTime = +new Date;
       // switch back and forth for 60 seconds.
       setInterval(function () {
-        if ((+new Date) - startTime > 60 * 1000)
+        if ((+new Date) - startTime > 60 * 3 * 1000)
           return;
 
         page1();
@@ -69,22 +74,3 @@ if (Meteor.isClient) {
 
 
 }
-
-// create collection
-DB = {};
-DB.collection = new Meteor.Collection(null);
-
-
-
-
-
-
-
-
-// dont need that
-
-// if (Meteor.isServer) {
-//   Meteor.startup(function () {
-//     // code to run on server at startup
-//   });
-// }
